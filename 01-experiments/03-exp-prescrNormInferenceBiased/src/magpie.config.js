2464fb5e620f2a956da5c21e9dc3b7f5d460a4c7
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-08-12 22:17:41 +0200

    revert to debug

--- 01-experiments/03-exp-prescrNormInferenceBiased/src/magpie.config.js
+++ 01-experiments/03-exp-prescrNormInferenceBiased/src/magpie.config.js
@@ -3,9 +3,9 @@ export default {
   serverUrl: 'https://magpie-cogsciprag.fly.dev',
   socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C16QRRN4',
+  completionUrl: '',
   contactEmail: 'michael.franke@uni-tuebingen.de',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'debug',
   language: 'en'
 };
