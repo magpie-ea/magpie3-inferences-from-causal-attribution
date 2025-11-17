d9de41552a6f72191a9d571e46605be6283c2390
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-11-17 12:46:09 +0100

    restructure repo

---
+++ 01-experiments/02-phase-listener-oriented/02-exp-statNormInference-pilot-1/src/magpie.config.js
@@ -0,0 +1,11 @@
+export default {
+  experimentId: '24',
+  serverUrl: 'https://magpie-cogsciprag.fly.dev//',
+  socketUrl: 'wss://magpie-cogsciprag.fly.dev//socket',
+  // this will be used in prolific mode
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=C1NZKJRA',
+  contactEmail: 'michael.franke@uni-tuebingen.de',
+  // Either 'debug', 'directLink' or 'prolific'
+  mode: 'prolific',
+  language: 'en'
+};
