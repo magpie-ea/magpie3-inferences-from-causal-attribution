9422aa6842c426c974efa708f527175ba1d02ae6
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-12-23 11:54:29 +0100

    formatting fix
    Added HTML paragraph tags for better formatting.

--- 01-experiments/01-phase-pilot/01-exp-descrNormInference/src/App.vue
+++ 01-experiments/01-phase-pilot/01-exp-descrNormInference/src/App.vue
@@ -84,13 +84,15 @@
     </template>
 
     <InstructionScreen :title="'Further Instructions'">
-      There are two religions on the island.
+      <p>There are two religions on the island.<br>
       In religion 1, the color Red is thought to be impure---for example, believers in Religion 1 avoid wearing red clothes.
       So when they use the marble machine, they think `Red marbles should not be released’.
-      <br><br>
-      In religion 2, the color Blue is thought to be sinful---for example, believers in Religion 2 avoid using blue paint in their artwork.
+      </p>
+      
+      <p>In religion 2, the color Blue is thought to be sinful---for example, believers in Religion 2 avoid using blue paint in their artwork.
       So when they use the marble machine, they think `Blue marbles should not be released’.
-      <br><br>
+      </p>
+      <br>
 
       <p>
         <img src="images/color-preferences.png" />
