5bf3796b021eee797388b4bf957681dc8c1c9bad
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2025-12-30 18:36:50 +0100

    exp1,2,3: prepare for pilot01

--- 01-experiments/03-phase-speaker-and-listener-oriented/02-exp-statisNorm-speakerListener/src/magpie.config.js
+++ 01-experiments/03-phase-speaker-and-listener-oriented/02-exp-statisNorm-speakerListener/src/magpie.config.js
@@ -3,9 +3,9 @@ experimentId: '41',
 serverUrl: 'https://magpie-cogsciprag.fly.dev',
 socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
   // this will be used in prolific mode
-  completionUrl: 'https://...',
+  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CHGWIA0M',
   contactEmail: 'michael.franke@uni-tuebingen.de',
   // Either 'debug', 'directLink' or 'prolific'
-  mode: 'directLink',
+  mode: 'prolific',
   language: 'en'
 };
