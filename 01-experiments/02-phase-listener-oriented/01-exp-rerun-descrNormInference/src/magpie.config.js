38d9803dd000b8a5e18fd4c7f5c4c9365eacaa70
Erik Zeiner <47943924+ErikZeiner@users.noreply.github.com>
2026-06-09 11:03:57 +0200

    set up Exp1 for CCL class demo

--- 01-experiments/02-phase-listener-oriented/01-exp-rerun-descrNormInference/src/magpie.config.js
+++ 01-experiments/02-phase-listener-oriented/01-exp-rerun-descrNormInference/src/magpie.config.js
@@ -1,11 +1,11 @@
 export default {
-  experimentId: '23',
-  serverUrl: 'https://magpie-cogsciprag.fly.dev//',
-  socketUrl: 'wss://magpie-cogsciprag.fly.dev//socket',
-  // this will be used in prolific mode
-  completionUrl: 'https://app.prolific.com/submissions/complete?cc=CH7R9CT2',
-  contactEmail: 'michael.franke@uni-tuebingen.de',
-  // Either 'debug', 'directLink' or 'prolific'
-  mode: 'prolific',
-  language: 'en'
+    experimentId: '70',
+    serverUrl: 'https://magpie-cogsciprag.fly.dev',
+    socketUrl: 'wss://magpie-cogsciprag.fly.dev/socket',
+    // this will be used in prolific mode
+    completionUrl: '',
+    contactEmail: 'michael.franke@uni-tuebingen.de',
+    // Either 'debug', 'directLink' or 'prolific'
+    mode: 'directLink',
+    language: 'en'
 };
